3a5b04344ea4293572380ae48ca1189c634b6afd
RAHUL02986 <kapoorrahul304@gmail.com>
2025-11-11 15:31:29 +0530

    first commit

--- app/components/Home/Features.tsx
+++ app/components/Home/Features.tsx
@@ -10,7 +10,7 @@ export default function Features() {
   return (
     <section className="sm:py-17 pt-10 md:pt-16  px-3 xl:mx-0 sm:mx-5 bg-[#FAFDFF]">
       <div className="container">
-        <div className="flex flex-col items-center justify-center w-full bg-[#FFFFFF] sm:py-7.5 py-5 rounded-full border border-[#0070F3]">
+        <div className="flex flex-col items-center justify-center w-full bg-[#FFFFFF] sm:py-7.5 py-5 px-4 rounded-full border border-[#0070F3]">
           <h2 className="md:mb-3 mb-2 text-center md:text-4xl text-2xl">
             Palmpe™ — Palmpay™
           </h2>
